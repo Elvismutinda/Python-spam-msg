cefb0a5d004660b49b0fe6cefa4b07f76f6fe9f8
Elvocool <elvocool@gmail.com>
2023-04-25 15:37:32 +0300

    update

--- spam.py
+++ spam.py
@@ -8,8 +8,12 @@ i = 0
 
 time.sleep(10)
 
-while 1<int(limit):
-    spam.typewrite(msg)
+for i in range(0, int(limit)):
+    msg += '.\n'
+    spam.typewrite(msg)                     # ----> Sends exact number of messages
     spam.press('Enter')
     
-    i+=1
+    # i+=1           ----> This sends messages as factorial
+
+
+# while 1<int(limit)         ----> This sends infinite messages
